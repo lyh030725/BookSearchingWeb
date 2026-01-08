e900b2b6e8f0a08bf67bc90c9a74e5601dc66c1c
YongHa <lyh030725@gmail.com>
2026-01-08 13:44:44 +0900

    complete

---
+++ src/lib/fetch-one-book.ts
@@ -0,0 +1,16 @@
+import { BookData } from "@/types";
+
+export default async function fetchOneBook(
+  id: number
+): Promise<BookData | null> {
+  const url = `https://onebite-books-server-six-gamma.vercel.app/book/${id}`;
+
+  try {
+    const response = await fetch(url);
+    if (!response.ok) throw new Error();
+    return response.json();
+  } catch (err) {
+    console.error(err);
+    return null;
+  }
+}
